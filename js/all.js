230144070f7da44e13bd4fe174f11b6268534782
wxywr <1261046195@qq.com>
2017-12-15 20:00:06 +0800

    update

--- js/all.js
+++ js/all.js
@@ -1,4 +1,18 @@
 $(function(){
+	$('.login li .username').focus(function(){
+		$(this).css('border','1px solid #5EA8E5');
+	});
+	$('.login li .username').blur(function(){
+		$(this).css('border','1px solid #ccc');
+	});
+	
+	$('.login li .password').focus(function(){
+		$(this).css('border','1px solid #5EA8E5');
+	});
+	$('.login li .password').blur(function(){
+		$(this).css('border','1px solid #ccc');
+	});
+
 	$('.btn').click(function(){
 		var name = $('.username').val();
 		var psd = $('.password').val();
@@ -10,6 +24,62 @@ $(function(){
 			alert("请输入密码");
 			return false;
 		}
-		// window.location.href="";
+		window.location.href="student-list.html";
 	})
+
+	// 删除
+	$('.list-wrap .wrap-l li i').click(function(){
+		if(window.confirm('确定要删除吗？')){
+	        $(this).parent().remove();
+	            return true;
+	        }else{
+	            return false;
+	        }
+	})
+
+	// 添加学生
+	$('.list-wrap .wrap-l .add').click(function(){
+		$('.layout').css('display','block');
+	})
+
+
+	$('.layout .add-stu .button').click(function(){
+		var name = $('input[name="name"]').val();
+		var phone = $('input[name="phone"]').val();
+		
+		if(name=="" || phone==null){
+			alert("请输入姓名");
+			return false;
+		}
+		if(!(/^1[3|4|5|7|8][0-9]{9}$/.test(phone))){
+			alert('请输入正确的手机号码');
+			return false;
+		}
+
+
+		$('.layout').css('display','none');
+		
+		$('#ul-a').append('<li><i></i></li>');
+	})
+
+	// 修改信息
+	$('.xiu').click(function(){
+		$('.Popup').css('display','block');
+	})
+
+	$('.Popup .add-stu .button').click(function(){
+		var pho = $('input[name="pho"]').val();
+		
+		if(!(/^1[3|4|5|7|8][0-9]{9}$/.test(pho))){
+			alert('请输入正确的手机号码');
+			return false;
+		}
+
+		$('.Popup').css('display','none');
+
+		$(".zj").text(pho);
+	})
+
+
+
 })
---
+++ js/index.js
@@ -0,0 +1,79 @@
+var date = new Date();
+date  = date.valueOf()
+date  = date  - 4 * 24 * 60 * 60 * 1000;
+date  = new Date(date);
+
+year = date.getFullYear();
+month = date.getMonth() + 1;
+date = date.getDate();
+document.getElementById("HiddenDate").innerText = year + "/" + month + "/" + date + " 00:00:00";
+InitDate();
+
+function Choose(obj)
+{
+var id = obj.id;
+for(var i = 1;i < 8;i++)
+{
+document.getElementById("custom" + i).className = "";
+}
+document.getElementById(id).className = "customChoose";
+}
+
+function TurnLeft()
+{
+var date = new Date(document.getElementById("HiddenDate").innerText);
+date  = date.valueOf()
+date  = date  - 9 * 24 * 60 * 60 * 1000;
+date  = new Date(date);
+year = date.getFullYear();
+month = date.getMonth() + 1;
+date = date.getDate();
+document.getElementById("HiddenDate").innerText = year + "/" + month + "/" + date + " 00:00:00";
+InitDate();
+}
+
+function TurnRight()
+{
+var date = new Date(document.getElementById("HiddenDate").innerText);
+date  = date.valueOf()
+date  = date  + 9 * 24 * 60 * 60 * 1000;
+date  = new Date(date);
+year = date.getFullYear();
+month = date.getMonth() + 1;
+date = date.getDate();
+document.getElementById("HiddenDate").innerText = year + "/" + month + "/" + date + " 00:00:00";
+InitDate();
+}
+
+function InitDate()
+{
+for(var i = 1;i < 8;i++)
+{
+document.getElementById("custom" + i).className = "";
+}
+document.getElementById("custom5").className = "customChoose";
+for(var i = 1;i <= 7;i++)
+{
+date = document.getElementById("HiddenDate").innerText;
+document.getElementById("custom" + i).innerText = GetDate(date,i - 1);
+}
+
+}
+
+function GetDate(currentDate,num)
+{
+var date = new Date(currentDate);
+date  = date.valueOf()
+date  = date + num * 24 * 60 * 60 * 1000;
+date  = new Date(date);
+this.year = date.getFullYear();
+this.month = date.getMonth() + 1;
+this.date = date.getDate();
+this.day = new Array("星期天", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六")[date.getDay()];
+this.hour = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
+this.minute = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+this.second = date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
+var currentTime = this.month + "月" + this.date + "\n"+ this.day;
+return currentTime;
+
+}
