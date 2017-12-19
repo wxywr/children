2d3ae8422a12eb922852cf64e60516cfa864adca
wxywr <1261046195@qq.com>
2017-12-19 18:16:02 +0800

    update

--- js/all.js
+++ js/all.js
@@ -26,23 +26,15 @@ $(function(){
 		}
 		window.location.href="student-list.html";
 	})
+	
 
-	// 删除
-	$('.list-wrap .wrap-l li i').click(function(){
-		if(window.confirm('确定要删除吗？')){
-	        $(this).parent().remove();
-	            return true;
-	        }else{
-	            return false;
-	        }
-	})
-
-	// 添加学生
+	
 	$('.list-wrap .wrap-l .add').click(function(){
 		$('.layout').css('display','block');
 	})
 
 
+// 添加学生
 	$('.layout .add-stu .button').click(function(){
 		var name = $('input[name="name"]').val();
 		var phone = $('input[name="phone"]').val();
@@ -58,8 +50,21 @@ $(function(){
 
 
 		$('.layout').css('display','none');
-		
-		$('#ul-a').append('<li><i></i></li>');
+
+		$('#ul-a').append('<li class="names"></li>');
+		$('.names').html($('input[name="name"]').val());
+		$('.names').append('<i class="i-icon"></i>');
+
+		// 删除
+		$('.i-icon').on('click',function(){
+			if(window.confirm('确定要删除吗？')){
+				$(this).parent().remove();
+				return true;
+			}
+			else{
+				return false;
+			}
+		})
 	})
 
 	// 修改信息
@@ -81,4 +86,93 @@ $(function(){
 	})
 
 
+	// 关闭弹层
+	$('.Popup .close').click(function(){
+		$('.Popup').css('display','none');
+	})
+	$('.layout .close').click(function(){
+		$('.layout').css('display','none');
+	})
+
+	// 提交亲子表单
+	$('.gallery-top .Submit').click(function(){
+		var sw = $('input[name="morning"]').val();
+		var xw = $('input[name="afternoon"]').val();
+		var sc = $('input[name="am"]:checked').val();
+		var xc = $('input[name="pm"]:checked').val();
+		var zs = $('input[name="zhu"]').val();
+		var sa = $('input[name="shu"]').val();
+		var ta = $('input[name="soup"]').val();
+		var jy = $('input[name="sug"]').val();
+		var zh = $('input[name="zhus"]:checked').val();
+		var su = $('input[name="su"]:checked').val();
+		var tg = $('input[name="tg"]:checked').val();
+		var xi = $('input[name="wa"]:checked').val();
+		var ci = $('input[name="bu"]').val();
+		var jw = $('input[name="jw"]:checked').val();
+		var take =[];
+		$('.bring input[type="checkbox"]:checked').each(function(){ 
+            take.push($(this).val()); 
+        });
+		if(sw==''){
+			alert("请输入上午的点心");
+			return false
+		}
+		if(xw==''){
+			alert("请输入下午的点心");
+			return false
+		}
+		if(zs==''){
+			alert("请输入主食");
+			return false
+		}
+		if(sa==''){
+			alert("请输入蔬菜名");
+			return false
+		}
+		if(ta==''){
+			alert("请输入汤名");
+			return false
+		}
+		if(jy==''){
+			alert("请输入老师的建议");
+			return false
+		}
+		if(ci==''){
+			alert("请输入换尿布次数");
+			return false
+		}
+
+	
+		$.ajax({
+			type: "post",
+			url: " ",
+			data: {
+				"sw": sw,
+				"xw": xw,
+				"sc": sc,
+				"xc": xc,
+				"zs": zs,
+				"sa": sa,
+				"ta": ta,
+				"jy": jy,
+				"zh": zh,
+				"su": su,
+				"tg": tg,
+				"xi": xi,
+				"ci": ci,
+				"jw": jw,
+				"take": take.toString(),
+			},
+			success : function(){
+				
+			},
+			error : function(){
+				alert("页面出错！");
+			}
+		});
+
+	});
+
 })
+
